24eeeada47d830e086bb7df4cad3d98d1b3fa107
WebDeveloperSrabon <122147793+WebDeveloperSrabon@users.noreply.github.com>
2023-01-14 23:57:36 -0800

    fist

---
+++ slick-1.8.1/slick/main.js
@@ -0,0 +1,65 @@
+$('.mainTestimonial').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 1100,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 1300,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 1400,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+
+    ]
+  });
